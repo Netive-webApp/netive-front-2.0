a6671cc2354d9cdea8c69f395d56d93cf6feca91
Vatsal Shukla <vatsalshukla2001@gmail.com>
2022-03-21 00:31:39 +0530

    main

--- components/login.jsx
+++ components/login.jsx
@@ -35,7 +35,7 @@ const Login = () => {
               <div className="rounded-t mb-0 px-6 py-6">
                 <div className="text-center mb-3">
                   <h6 className="text-blueGray-500 text-sm font-bold">
-                    Sign in with
+                    Sign in
                   </h6>
                 </div>
                 {/*<div className="btn-wrapper text-center">
@@ -51,9 +51,9 @@ const Login = () => {
                 <hr className="mt-6 border-b-1 border-blueGray-300" />
               </div>
               <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                <div className="text-blueGray-400 text-center mb-3 font-bold">
+                {/*<div className="text-blueGray-400 text-center mb-3 font-bold">
                   <small>Or sign in with credentials</small>
-                </div>
+                </div>*/}
                 <form onSubmit="">
                   {/* {handleSubmit(onSubmit)} */}
                   <div className="relative w-full mb-3">
